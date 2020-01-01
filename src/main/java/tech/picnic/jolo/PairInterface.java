147463966bd6e95e8a6a71c44dffec4136aa5785
Stephan Schroevers <stephan.schroevers@teampicnic.com>
2020-01-01 21:07:59 +0100

    Add some documentation

--- src/main/java/tech/picnic/jolo/PairInterface.java
+++ src/main/java/tech/picnic/jolo/PairInterface.java
@@ -2,6 +2,7 @@ package tech.picnic.jolo;
 
 import org.immutables.value.Value;
 
+/** A pair of related IDs. */
 @Value.Immutable(builder = false)
 @Value.Style(
     allParameters = true,
@@ -9,7 +10,9 @@ import org.immutables.value.Value;
     typeImmutable = "*",
     visibility = Value.Style.ImplementationVisibility.PUBLIC)
 interface PairInterface {
+  /** The left-hand ID of the relation. */
   long getLeftId();
 
+  /** The right-hand ID of the relation. */
   long getRightId();
 }
